ef18e13de3242b4eed475a455f4e55258e8e61d1
saad484 <aiddisaad484@gmail.com>
2024-06-26 00:45:57 +0100

    return a tuple containing the start index and the end index

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+index_range return a tuple of size two containing a start and an end index 
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int):
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
