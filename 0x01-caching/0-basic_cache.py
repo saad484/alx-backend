64305c5ce74391734eefece28bb9ee9dbd34acfa
saad484 <aiddisaad484@gmail.com>
2024-06-27 21:33:08 +0100

    get and put items in a dict

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
-"""
+"""Basic caching module.
 """
 from base_caching import BaseCaching
 
 
-def BasicCache(BaseCaching):
-    """
-    Object that allows storing and retirieving items
-    from a dictionary
+class BasicCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary.
     """
     def put(self, key, item):
-        """
-        Adds an item in the cache
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Retrieve an item by key
+        """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
