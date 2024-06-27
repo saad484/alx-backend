2c0a8705c83185cfb3ebf3a01ffca55925be98dc
saad484 <aiddisaad484@gmail.com>
2024-06-27 21:58:18 +0100

    fifo cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+def FIFOCache(BaseCaching):
+    """
+    Rerpresents an Object that allows storing and retrieving items
+    from a dictionary with a FIFO removal mechanism when the limit is
+    reached
+    """
+    def __init__(self):
+        """
+        Initializes the cache
+        """
+        super.cache_data() = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Adds an item in the cache
+        """
+        if the key is None or items is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print('DISCARD:', first_key)
+
+    def get(self, key):
+        """
+        retrieves an item by key
+        """
+        return self.cache_data.get(key, None)
