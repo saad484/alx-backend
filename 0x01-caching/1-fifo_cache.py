1b8013390ab1614cfa389bc56b764a05ec6a265e
saad484 <aiddisaad484@gmail.com>
2024-06-28 16:46:53 +0100

    fifo caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-DOC DOC Object
+Fifo Caching algo
 """
 from base_caching import BaseCaching
 from collections import OrderedDict
 
 
-def FIFOCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """
     Rerpresents an Object that allows storing and retrieving items
     from a dictionary with a FIFO removal mechanism when the limit is
@@ -16,13 +16,14 @@ def FIFOCache(BaseCaching):
         """
         Initializes the cache
         """
-        super.cache_data() = OrderedDict()
+        super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
         Adds an item in the cache
         """
-        if the key is None or items is None:
+        if key is None or item is None:
             return
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
