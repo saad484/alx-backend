031cd344026dfe26dce071e131ce537ec2c59f55
saad484 <aiddisaad484@gmail.com>
2024-06-28 17:54:27 +0100

    lru caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -29,8 +29,8 @@ class LRUCache(BaseCaching):
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 lru_key, _ = self.cache_data.popitem(True)
                 print('DISCARD:', lru_key)
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=False)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
         else:
             self.cache_data[key] = item
 
